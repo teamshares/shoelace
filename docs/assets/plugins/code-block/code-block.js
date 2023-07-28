571f3509cd727e4d136599ca20f42379a4b0e8cc
Michael Daross <daross@teamshares.com>
2023-07-28 12:23:21 -0500

    Update code-block.js

--- docs/assets/plugins/code-block/code-block.js
+++ docs/assets/plugins/code-block/code-block.js
@@ -345,42 +345,11 @@
 
       // `<script type="module" src="https://cdn.jsdelivr.net/npm/@teamshares/shoelace@${version}/dist/shoelace.js"></script>\n` +
       // HTML templates
-      // TODO: Once we cut a new release with the registerExternalLibraries util, the JS template below can be replaced
       if (!isReact) {
         htmlTemplate = `${slimExample}`;
-        jsTemplate = `
-          import { registerIconLibrary } from 'https://cdn.jsdelivr.net/npm/@teamshares/shoelace@${version}/dist/utilities/icon-library.js';\n
-          \n
-          registerIconLibrary('fa-free', {
-            resolver: name => {
-              const filename = name.replace(/^fa[rbs]-/, '');
-              let folder = 'regular';
-              if (name.substring(0, 4) === 'fas-') folder = 'solid';
-              if (name.substring(0, 4) === 'fab-') folder = 'brands';
-              return \`https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.3.0/svgs/\${folder}/\${filename}.svg\`;
-            },
-            mutator: svg => svg.setAttribute('fill', 'currentColor')
-          });\n
-          \n
-          registerIconLibrary('fa', {
-            resolver: name => {
-              const filename = name.replace(/^fa([rsltdb]|(ss))-/, '');
-              const sub = name.substring(0, 4);
-              const folderHash = {
-                'fas-': 'solid',
-                'fal-': 'light',
-                'fat-': 'thin',
-                'fad-': 'duotone',
-                'fab-': 'brands'
-              };
-              const folder = folderHash[sub] || 'regular';
-              /* Note: The token refers to the Teamshares Font Awesome Kit */
-              /* See https://fontawesome.com/kits/44da2a9d09/setup */
-              return \`https://ka-p.fontawesome.com/releases/v6.4.0/svgs/\${folder}/\${filename}.svg?token=44da2a9d09\`;
-            },
-            mutator: svg => svg.setAttribute('fill', 'currentColor')
-          });\n
-        `;
+        jsTemplate =
+          `import { registerExternalLibraries } from 'https://cdn.jsdelivr.net/npm/@teamshares/shoelace@${version}/dist/utilities/icon-library.js';\n` +
+          `registerExternalLibraries();`;
       }
 
       // React templates
