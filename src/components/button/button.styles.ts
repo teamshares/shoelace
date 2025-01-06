20f164c3226e7c7907fc16469ab8bd2d979a1f60
Sara <slhinyc@users.noreply.github.com>
2025-01-06 18:10:19 -0500

    Update styling for `success` variant of `outline` button

--- src/components/button/button.styles.ts
+++ src/components/button/button.styles.ts
@@ -255,20 +255,20 @@ export default css`
 
   /* Success */
   .button--outline.button--success {
-    border-color: var(--sl-color-success-600);
-    color: var(--sl-color-success-600);
+    border-color: var(--sl-color-success-200);
+    color: var(--sl-color-success-700);
   }
 
   .button--outline.button--success:hover:not(.button--disabled),
   .button--outline.button--success.button--checked:not(.button--disabled) {
-    background-color: var(--sl-color-success-600);
-    color: var(--sl-color-neutral-0);
+    background-color: var(--sl-color-success-100);
+    color: var(--sl-color-success-700);
   }
 
   .button--outline.button--success:active:not(.button--disabled) {
-    border-color: var(--sl-color-success-700);
-    background-color: var(--sl-color-success-700);
-    color: var(--sl-color-neutral-0);
+    border-color: var(--sl-color-success-200);
+    background-color: var(--sl-color-success-200);
+    color: var(--sl-color-success-700);
   }
 
   /* Neutral */
