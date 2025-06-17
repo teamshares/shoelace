5f39e7a92c6a55f0df1586f7ab943eef1862ecd7
Sara <slhinyc@users.noreply.github.com>
2025-06-17 19:36:55 -0400

    Remove all from mistaken PR

--- src/utilities/icon-library.ts
+++
@@ -1,1 +0,0 @@
-export { registerIconLibrary, unregisterIconLibrary, registerExternalLibraries } from '../components/icon/library.js';
--- src/vendor/index.js
+++
@@ -1,5 +0,0 @@
-// Import (and register, if necessary) 3rd-party web components here.
-
-import DataGrid from 'data-grid-component';
-
-export default DataGrid;
