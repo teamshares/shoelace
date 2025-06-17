c6bd1e5b20c50081f4792c7142d0932b6ebb8d7d
Sara <slhinyc@users.noreply.github.com>
2025-06-16 23:01:15 -0400

    Revert "😢"
    This reverts commit 2785f3d0fb95b80f561ba988df60cd807a6506eb.

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -24,9 +24,8 @@ export default {
   ],
   browsers: [
     playwrightLauncher({ product: 'chromium' }),
-    playwrightLauncher({ product: 'webkit' })
-    // Skipping Firefox for now due to random test failures on CI.
-    // playwrightLauncher({ product: 'firefox' })
+    playwrightLauncher({ product: 'webkit' }),
+    playwrightLauncher({ product: 'firefox' })
   ],
   testRunnerHtml: testFramework => `
     <html lang="en-US">
